239e63a33dcc2e31020bbd0233d48d9e67451f41
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-15 22:26:23 +0100

    New certificate

---
+++ renderer/app/[locale]/certificates/webinar-ngia-20240514-spk/page.js
@@ -0,0 +1,9 @@
+/* * */
+
+import CertificatesWebinarNgia20240514Spk from '@/components/CertificatesWebinarNgia20240514Spk';
+
+/* * */
+
+export default function Page() {
+  return <CertificatesWebinarNgia20240514Spk />;
+}
--- renderer/next.config.js
+++ renderer/next.config.js
@@ -6,13 +6,4 @@ const withNextIntl = require('next-intl/plugin')();
 
 module.exports = withNextIntl({
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/geobus',
-        permanent: false,
-      },
-    ];
-  },
 });
